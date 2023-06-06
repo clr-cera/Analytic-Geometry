1a27fde8ac21a248c17453d41514f538f362795e
clr-cera <clr-cera@users.noreply.github.com>
2023-06-06 15:51:15 -0300

    little changes

--- vectors.h
+++ vectors.h
@@ -5,10 +5,13 @@ double innerprod(double *vec1, double *vec2);
 
 double module(double *vec);
 
+double pointDistance(double *p1, double *p2);
+
 typedef double vector3[3];
 
+typedef double point3[3];
 
-double innerprod(double* vec1, double* vec2){
+double innerprod(vector3 vec1, vector3 vec2){
   double result = 0;
 
   for (int i = 0; i < 3; i++){
@@ -18,7 +21,7 @@ double innerprod(double* vec1, double* vec2){
   return (result); 
 }
 
-double* vecprod(double* vec1, double* vec2){
+double* vecprod(vector3 vec1, vector3 vec2){
   double* result; 
 
 
@@ -28,6 +31,17 @@ double* vecprod(double* vec1, double* vec2){
   return result;
 }
 
-double module(double* vec){
+double module(vector3 vec){
   return (sqrt(innerprod(vec,vec)));
 }
+
+double pointDistance(point3 p1, point3 p2){
+  double sum = 0;
+  
+  for (int i = 0; i < 3; i++)
+    sum += pow(p1[i] - p2[i], 2);
+  
+  return(sqrt(sum));
+}
+
+
