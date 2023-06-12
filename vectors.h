ba324e938bed33f39f941ae47eb82913ef1435e9
clr-cera <clr-cera@users.noreply.github.com>
2023-06-12 15:53:03 -0300

    added some comment and stuff, am thinking of waiting more between commits

--- vectors.h
+++ vectors.h
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-/*Functions*/
+/*Function declarations*/
 double innerprod(double *vec1, double *vec2);
 
 double module(double *vec);
@@ -10,10 +10,17 @@ double pointDistance(double *p1, double *p2);
 
 double* vecprod(double *vec1, double *vec2);
 
+
+/*Type definitions*/
 typedef double vector3[3];
 
 typedef double point3[3];
 
+
+/*All functions*/
+
+
+/*Inner product function for 3 dimension vectors*/
 double innerprod(vector3 vec1, vector3 vec2){
   double result = 0;
 
@@ -24,9 +31,12 @@ double innerprod(vector3 vec1, vector3 vec2){
   return (result); 
 }
 
+/*Vectorial product function*/
 double* vecprod(vector3 vec1, vector3 vec2){
   double* result;
-  result  = (double *) calloc(3, sizeof("double"));
+
+  result  = (double *) malloc(3 * sizeof("double"));
+  if (result == NULL) exit(1);
 
   result[0] = (vec1[1] * vec2[2]) - (vec1[2] * vec2[1]);
   result[1] = (vec1[2] * vec2[0]) - (vec1[0] * vec2[2]);
@@ -35,10 +45,12 @@ double* vecprod(vector3 vec1, vector3 vec2){
   return result;
 }
 
+/*Vector module for 3 dimension vectors*/
 double module(vector3 vec){
   return (sqrt(innerprod(vec,vec)));
 }
 
+/*Distance between two points in R3*/
 double pointDistance(point3 p1, point3 p2){
   double sum = 0;
   
